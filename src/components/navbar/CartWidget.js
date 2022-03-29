68cc9f5ac3f2166d4cff984e78286ee058d9af08
Ary Demitropulos <demitropulosary@gmail.com>
2022-03-28 21:45:55 -0300

    Se completa el Desafío: Cart View

--- src/components/navbar/CartWidget.js
+++ src/components/navbar/CartWidget.js
@@ -5,17 +5,20 @@ import { Link } from "react-router-dom";
 
 import { context } from "../../context/CartContext";
 import { useContext } from "react";
+import Conditional from "../Conditional";
 
 const CartWidget = () => {
   const { getTotalItems } = useContext(context);
   return (
-    <Link to="/cart">
-      <IconButton size="large" aria-label="show 4 cart" color="inherit">
-        <Badge badgeContent={getTotalItems()} color="error">
-          <ShoppingCartIcon />
-        </Badge>
-      </IconButton>
-    </Link>
+    <Conditional condition={getTotalItems()}>
+      <Link to="/cart">
+        <IconButton size="large" aria-label="show 4 cart" color="inherit">
+          <Badge badgeContent={getTotalItems()} color="error">
+            <ShoppingCartIcon />
+          </Badge>
+        </IconButton>
+      </Link>
+    </Conditional>
   );
 };
 
